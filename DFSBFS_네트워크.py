0ea45af434c942d04d81a6c7e0d871060a6bf7a8
SooYeon Yeon <75442105+dustndus8@users.noreply.github.com>
2022-03-28 22:36:38 +0900

    feat: DFSBFS_네트워크.py

---
+++ DFSBFS_네트워크.py
@@ -0,0 +1,16 @@
+def solution(n, computers):
+    answer = 0
+    visited=[False for i in range(n)]
+    for com in range(n):
+        if visited[com]==False:
+            dfs(n,computers,com,visited)
+            answer+=1
+            
+    return answer
+def dfs(n,computers,com,visited):
+    visited[com]=True
+    for i in range(n):
+            if i!=com and computers[com][i]==1:
+                if visited[i]==False:
+                    dfs(n,computers,i,visited)
+    
